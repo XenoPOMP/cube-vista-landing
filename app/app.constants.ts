5c212f77df9e6a49880b913c267ac2a69a0e7c38
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-22 00:01:56 +0300

    add maxWidth parameter to UiContainer

--- app/app.constants.ts
+++ app/app.constants.ts
@@ -1,6 +1,6 @@
 import IAppConstants from '@/src/interfaces/AppConstants.interface';
 
 export const AppConstants: IAppConstants = {
-  appName: 'Next Template',
-  maxContainerWidth: '1680px',
+  appName: 'Cube Vista',
+  maxContainerWidth: '1800px',
 };
