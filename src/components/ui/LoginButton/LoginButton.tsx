03853e75c9c7201d638808d756f09645e640494f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-23 13:13:41 +0300

    change font sizes in header

--- src/components/ui/LoginButton/LoginButton.tsx
+++ src/components/ui/LoginButton/LoginButton.tsx
@@ -10,8 +10,8 @@ const LoginButton: FC<LoginButtonProps> = ({}) => {
       <span>Войти</span>
 
       <svg
-        width='1em'
-        height='1.03em'
+        width='1.5em'
+        height='1.55em'
         viewBox='0 0 30 31'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
@@ -24,10 +24,10 @@ const LoginButton: FC<LoginButtonProps> = ({}) => {
           maskUnits='userSpaceOnUse'
           x='0'
           y='0'
-          width='1em'
-          height='1.03em'
+          width='1.5em'
+          height='1.55em'
         >
-          <rect y='0.5' width='1em' height='1em' fill='#D9D9D9' />
+          <rect y='0.5' width='1.5em' height='1.5em' fill='#D9D9D9' />
         </mask>
         <g mask='url(#mask0_2_69)'>
           <path d='M15 26.75V24.25H23.75V6.75H15V4.25H23.75C24.4375 4.25 25.026 4.49479 25.5156 4.98438C26.0052 5.47396 26.25 6.0625 26.25 6.75V24.25C26.25 24.9375 26.0052 25.526 25.5156 26.0156C25.026 26.5052 24.4375 26.75 23.75 26.75H15ZM12.5 21.75L10.7812 19.9375L13.9688 16.75H3.75V14.25H13.9688L10.7812 11.0625L12.5 9.25L18.75 15.5L12.5 21.75Z' />
