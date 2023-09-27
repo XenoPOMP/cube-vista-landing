3f7a88e8efeda3deb4e37ea6ff421d22a3f7fee8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-27 20:11:41 +0300

    create animation for online led

--- src/components/ui/OnlineBadge/OnlineBadge.tsx
+++ src/components/ui/OnlineBadge/OnlineBadge.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import cn from 'classnames';
+import { motion } from 'framer-motion';
 import { FC } from 'react';
 
 import styles from './OnlineBadge.module.scss';
@@ -11,9 +14,20 @@ const OnlineBadge: FC<OnlineBadgeProps> = ({
 }) => {
   return (
     <div className={cn(styles.onlineBadge, !isReversed && 'flex-row-reverse')}>
-      <svg viewBox='0 0 10 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <motion.svg
+        viewBox='0 0 10 11'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+        animate={{
+          opacity: [0, 1, 0],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 1.25,
+        }}
+      >
         <circle cx='5' cy='5.5' r='5' fill='#FC8B94' />
-      </svg>
+      </motion.svg>
 
       <span
         className={cn(styles.text)}
