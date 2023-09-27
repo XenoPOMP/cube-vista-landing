9069de68a1ed160937fdd2e6b72e5f9a143a2874
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-27 20:06:03 +0300

    wrap container in other one

--- src/sections/mainPage/PromoGallery/PromoGallery.tsx
+++ src/sections/mainPage/PromoGallery/PromoGallery.tsx
@@ -14,40 +14,42 @@ import { PromoGalleryProps } from './PromoGallery.props';
 const PromoGallery: FC<PromoGalleryProps> = ({}) => {
   return (
     <div className={cn(styles.galleryWrapper)}>
-      <UiContainer
-        className={cn(styles.container)}
-        horizontalAlignment={'right'}
-        margin={'1.5rem'}
-        maxWidth={'1860px'}
-      >
-        <article className={cn(styles.textBlock)}>
-          <OnlineBadge />
+      <UiContainer maxWidth={'1920px'} margin={'0px'}>
+        <UiContainer
+          className={cn(styles.container)}
+          horizontalAlignment={'right'}
+          margin={'1.5rem'}
+          maxWidth={'1860px'}
+        >
+          <article className={cn(styles.textBlock)}>
+            <OnlineBadge />
 
-          <div className={cn(styles.heading)}>
-            <h1>Майнкрафт Сервер для друзей</h1>
+            <div className={cn(styles.heading)}>
+              <h1>Майнкрафт Сервер для друзей</h1>
 
-            <p>
-              We have a propspering community and it`s growing faster than ever.
-              Suggest new features, discuss what`s new, and or share your
-              content
-            </p>
-          </div>
+              <p>
+                We have a propspering community and it`s growing faster than
+                ever. Suggest new features, discuss what`s new, and or share
+                your content
+              </p>
+            </div>
 
-          <Button
-            className={cn('text-[1.5em]', styles.button)}
-            variant={'secondary'}
-          >
-            <b>Начните играть</b> - это бесплатно!
-          </Button>
-        </article>
+            <Button
+              className={cn('text-[1.5em]', styles.button)}
+              variant={'secondary'}
+            >
+              <b>Начните играть</b> - это бесплатно!
+            </Button>
+          </article>
 
-        <article
-          className={cn(styles.gifBlock)}
-          style={{
-            background: `url(${stevePlaceholderArt.src}) center`,
-            backgroundSize: 'cover',
-          }}
-        ></article>
+          <article
+            className={cn(styles.gifBlock)}
+            style={{
+              background: `url(${stevePlaceholderArt.src}) center`,
+              backgroundSize: 'cover',
+            }}
+          ></article>
+        </UiContainer>
       </UiContainer>
     </div>
   );
